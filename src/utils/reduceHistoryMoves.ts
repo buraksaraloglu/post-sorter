897db2a133c9f3ff6e274661773100b14f1ef80e
Burak Saraloglu <buraksaraloglu1@gmail.com>
2022-06-13 07:07:57 +0300

    enhancement: remove unnecessery function call on reduceHistoryMoves

--- src/utils/reduceHistoryMoves.ts
+++ src/utils/reduceHistoryMoves.ts
@@ -33,67 +33,35 @@ export const reduceHistoryMoves = (history: IHistoryItem[]) => {
 
   const positions = new Map();
 
-  const cleanMoves = () => {
-    moves.forEach((move) => {
-      const newPosition = move.from + move.move;
+  moves.forEach((move) => {
+    const newPosition = move.from + move.move;
 
-      if (positions.has(newPosition)) {
-        const occupying = positions.get(newPosition);
+    if (positions.has(newPosition)) {
+      const occupying = positions.get(newPosition);
 
-        const moveOccupyingBy = (move.from < newPosition ? -1 : 1) * move.move;
-        if (newPosition === move.origin) {
-          moves.delete(move.id);
-          positions.delete(newPosition);
-
-          if (newPosition + moveOccupyingBy === occupying.origin) {
-            moves.delete(occupying.id);
-            return;
-          }
-
-          const newOccupying = { ...occupying, from: newPosition, move: occupying.move + moveOccupyingBy };
-          moves.set(occupying.id, newOccupying);
-          return;
-        }
-      }
-
+      const moveOccupyingBy = (move.from < newPosition ? -1 : 1) * move.move;
       if (newPosition === move.origin) {
         moves.delete(move.id);
+        positions.delete(newPosition);
+
+        if (newPosition + moveOccupyingBy === occupying.origin) {
+          moves.delete(occupying.id);
+          return;
+        }
+
+        const newOccupying = { ...occupying, from: newPosition, move: occupying.move + moveOccupyingBy };
+        moves.set(occupying.id, newOccupying);
         return;
       }
+    }
 
-      positions.set(newPosition, move);
-    });
-  };
+    if (newPosition === move.origin) {
+      moves.delete(move.id);
+      return;
+    }
 
-  cleanMoves();
+    positions.set(newPosition, move);
+  });
+
   return moves;
 };
-
-// if (!Array.isArray(history)) {
-//   throw new Error('State and history must be arrays');
-// }
-
-// if (!history.length) {
-//   return history;
-// }
-
-// const moves = new Map<IHistoryItem['id'], IHistoryMove>();
-
-// history.forEach((item) => {
-//   const previousMove = moves.get(item.id);
-//   if (!previousMove) {
-//     moves.set(item.id, { from: item.from, move: item.move });
-//     return;
-//   }
-
-//   const newMove = previousMove.move + item.move;
-
-//   if (newMove === 0) {
-//     moves.delete(item.id);
-//     return;
-//   }
-
-//   moves.set(item.id, { from: item.from, move: newMove });
-// });
-
-// return moves;
